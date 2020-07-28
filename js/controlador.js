0383fc9f9957cfb7e4af89450781f61138a12f8b
moyportillo <moyporti15@gmail.com>
2020-07-27 20:02:36 -0600

    vers. 1.7 menu hamburguesa con animacion

--- js/controlador.js
+++ js/controlador.js
@@ -1,50 +1,25 @@
-/*funciones para localizaciones*/
-function findMe() {
-    var output = document.getElementById('map');
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(localizacion, error);
-    } else {
-        output.innerHTML = "<p>Su navegador no soporta Geolocalización</p>";
-    }
-}
-
-function localizacion(posicion) {
-    var latitude = posicion.coords.latitude;
-    var longitude = posicion.coords.longitude;
-}
-
-function error() {
-    output.innerHTML = "<p>No se pudo obtener tu ubicación</p>";
-}
-
-function crearMapa() {
-  var map = new ol.Map({
-      layers: [
-          new ol.layer.Tile({
-              source: new ol.source.OSM()
-          })
-      ],
-      target: 'map',
-      view: new ol.View({
-          center: [0, 0],
-          zoom: 2
-      })
-  });
-  var view = new ol.View({
-      center: [41.56, -4.63],
-      zoom: 7
-  });
-  map.addControl(new ol.control.ZoomSlider());
-}
+var hamburgerBtn = null;
+var menu = null;
+var move = null;
+var isMenuHidden = true;
 
 document.addEventListener("DOMContentLoaded", page_load);
 
 function page_load(e){
-
-  document.getElementById("map").innerHTML = 
-    crearMapa();
+    hamburgerBtn = document.querySelector("header nav section");
+    menu = document.querySelector("header nav ul");
+    hamburgerBtn.addEventListener("click", hamburguer_clicked);
 }
 
-
-
-
+function hamburguer_clicked(e){
+    e.preventDefault();
+    e.stopPropagation();
+    if(isMenuHidden){
+        menu.className = "";
+    }
+    else{
+        menu.className = "hidden";
+        hamburgerBtn.className = "hamburguer move-menu";
+    }
+    isMenuHidden = !isMenuHidden;
+ }
